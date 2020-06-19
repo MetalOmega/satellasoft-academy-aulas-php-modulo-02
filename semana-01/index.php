8f581d28ccf879d2b4dca20f46231357ac1bd999
Metal Omega <metalomega@gmail.com>
2020-06-18 21:25:52 -0300

    Aula01 - Conhecendo o PHP

---
+++ semana-01/index.php
@@ -0,0 +1,17 @@
+<?php
+//phpinfo();
+
+$nome = 'Metal Omega';
+
+echo 'Metal Omega Simple <br>';
+echo "Metal Omega Double <br>";
+
+echo '$nome Simple <br>';
+echo "$nome Double <br>";
+
+echo '{$nome} Simple <br>';
+echo "{$nome} Double <br>";
+
+//contatenação
+echo $nome . ' Simple' . '<br>';
+echo "$nome Double <br>";
