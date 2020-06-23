e614cc578cbad3a2944c0954622adc8ed43677eb
Metal Omega <metalomega@gmail.com>
2020-06-22 23:02:13 -0300

    Aula04 - Composer

---
+++ semana-02/aula04/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once('vendor/autoload.php');
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
